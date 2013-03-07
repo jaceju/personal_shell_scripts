53a51748e58c5adb3c540dcc50d3a831ace491ae
jace ju <jaceju@gmail.com>
2013-03-07 14:09:06 +0800

    add speed per second

--- measure_website.sh
+++ measure_website.sh
@@ -8,6 +8,6 @@ do
     read -p "Please pass the url you want to measure: " URL
 done
 
-result=`$CURL -o /dev/null -s -w %{time_connect}:%{time_starttransfer}:%{time_total} "$URL"`
-echo -e "connect\tstart_transfer\ttotal "
-echo -e $result | $GAWK -F: '{ print $1"s\t"$2"s\t\t"$3"s"}'
+result=`$CURL -o /dev/null -s -w %{time_connect}:%{time_starttransfer}:%{time_total}:%{speed_download} "$URL"`
+echo -e "connect\tstart_transfer\ttotal\tspeed"
+echo -e $result | $GAWK -F: '{ print $1"s\t"$2"s\t\t"$3"s\t"$4"bytes/s"}'
